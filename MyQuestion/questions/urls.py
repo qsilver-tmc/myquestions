0273b65ed8784ab6acc598971310dce536b2af30
qrheart <qsilver.tmc@gmail.com>
2018-03-27 02:45:37 +0300

    upd.

--- MyQuestion/questions/urls.py
+++ MyQuestion/questions/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new/', views.new, name='new'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('settings/', views.settings, name='settings'),
+    path('question/', views.question, name='question'),
+    path('tag/', views.tag, name='tag'),
 ]
 
--- MyQuestion/questions/views.py
+++ MyQuestion/questions/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "index.html", {})
+
+def new(request):
+	return render(request, "new.html", {})
+
+def login(request):
+	return render(request, "login.html", {})
+
+def signup(request):
+	return render(request, "signup.html", {})
+
+def settings(request):
+	return render(request, "settings.html", {})
+
+def question(request):
+	return render(request, "question.html", {})
+
+def tag(request):
+	return render(request, "by_tag.html", {})
