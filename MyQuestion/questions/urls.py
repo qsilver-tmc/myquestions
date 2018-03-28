90f0322e03b04e8990807a5b73154d9c3c054f29
qsilver <qsilver.tmc@gmail.com>
2018-03-29 01:24:04 +0300

    subtempl upd

--- MyQuestion/questions/urls.py
+++ MyQuestion/questions/urls.py
@@ -4,11 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
+    path('ask/', views.ask, name='ask'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('settings/', views.settings, name='settings'),
-    path('question/', views.question, name='question'),
-    path('tag/', views.tag, name='tag'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('tag/<search_tag>/', views.tag, name='tag'),
+    path('hot/', views.hot, name='hot')
 ]
 
--- MyQuestion/questions/views.py
+++ MyQuestion/questions/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, "index.html", {})
+	questions = []
+	for i in range(0,5):
+  		questions.append({
+		'title': 'Как пропатчить kde2 под freebsd?',
+		'likes': 0,
+		'tags': {'kde2', 'freebsd'},
+		'answers': 1,
+		'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
+	context = {'questions': questions}
+	return render(request, "index.html", context)
 
-def new(request):
+def ask(request):
 	return render(request, "new.html", {})
 
 def login(request):
@@ -15,8 +24,43 @@ def signup(request):
 def settings(request):
 	return render(request, "settings.html", {})
 
-def question(request):
-	return render(request, "question.html", {})
+def question(request, question_id):
+	question = {
+		'title': 'Как пропатчить kde2 под freebsd?',
+		'likes': 0,
+		'tags': {'kde2', 'freebsd'},
+		'answers': 1,
+		'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+	
+	answers = []
+	for i in range(0,2):
+  		answers.append({
+		'likes': 0,
+		'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
 
-def tag(request):
-	return render(request, "by_tag.html", {})
+	context = {'question': question, 'answers': answers}
+	return render(request, "question.html", context)
+
+def tag(request, search_tag):
+	questions = []
+	for i in range(0,5):
+  		questions.append({
+		'title': 'Как пропатчить kde2 под freebsd?',
+		'likes': 0,
+		'tags': {'kde2', 'freebsd'},
+		'answers': 1,
+		'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
+	context = {'search_tag': search_tag, 'questions': questions};
+	return render(request, "by_tag.html", context)
+
+def hot(request):
+	questions = []
+	for i in range(0,5):
+  		questions.append({
+		'title': 'Как пропатчить kde2 под freebsd?',
+		'likes': 0,
+		'tags': {'kde2', 'freebsd'},
+		'answers': 1,
+		'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'})
+	context = {'questions': questions}
+	return render(request, "hot.html", context)
