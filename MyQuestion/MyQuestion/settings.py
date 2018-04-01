8c798efed63b6f2095db8fd7bf3bafca592bbe20
qsilver <qsilver.tmc@gmail.com>
2018-04-02 02:12:49 +0300

    fix

--- MyQuestion/MyQuestion/settings.py
+++ MyQuestion/MyQuestion/settings.py
@@ -25,10 +25,11 @@ SECRET_KEY = 's3*u=xjpqtu*f1s(@h(qy6bx=9kl2$d25pjao)vmg+@ha16m%5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1', 
-    'qsilvertmc.pythonanywhere.com'
-]
+ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS += ['qsilvertmc.pythonanywhere.com']
+ALLOWED_HOSTS += ['127.0.0.1']
+
 
 
 # Application definition
