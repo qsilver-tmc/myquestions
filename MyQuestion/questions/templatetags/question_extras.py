5f5f40d9796174f6772a39e6c343538b94099798
qrheart <qsilver.tmc@gmail.com>
2018-04-02 19:21:32 +0300

    fix

--- MyQuestion/questions/templatetags/question_extras.py
+++ MyQuestion/questions/templatetags/question_extras.py
@@ -1,12 +1,13 @@
 from django import template
 
 from questions.models import Tag, Profile
-
+from django.db.models import Count
 register = template.Library()
 
 @register.inclusion_tag('subtemplates/popular_tags.html')
 def show_popular_tags():
-	tags = Tag.objects.all()[:10]
+	tags = Tag.objects.annotate(q_count=Count('question')) \
+                                 .order_by('-q_count')[:10]
 	return {'tags': tags}
 
 @register.inclusion_tag('subtemplates/best_users.html')
