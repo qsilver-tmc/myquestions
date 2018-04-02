47fa0a2822f3c99bd834aa91cd4596283e6c4de2
qrheart <qsilver.tmc@gmail.com>
2018-04-02 18:55:03 +0300

    fix

---
+++ MyQuestion/questions/templatetags/question_extras.py
@@ -0,0 +1,15 @@
+from django import template
+
+from questions.models import Tag, Profile
+
+register = template.Library()
+
+@register.inclusion_tag('subtemplates/popular_tags.html')
+def show_popular_tags():
+	tags = Tag.objects.all()[:10]
+	return {'tags': tags}
+
+@register.inclusion_tag('subtemplates/best_users.html')
+def show_best_users():
+	profiles = Profile.objects.order_by('-rating')[:5]
+	return {'profiles': profiles}
